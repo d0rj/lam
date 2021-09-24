22ce9796596a75dd0f58349b8f7743e5fba55bc9
d0rj <hanaan@internet.ru>
2021-09-25 00:25:08 +0300

    refactor: remove unneccessary 'elif'

--- src/translater.py
+++ src/translater.py
@@ -18,10 +18,10 @@ def translate_value(tree_value: dict) -> str:
     value = tree_value['value']
     if 'number' in value:
         return str(value['number'])
-    elif 'string' in value:
+    if 'string' in value:
         return str(value['string'])
-    else:
-        raise Exception('Wrong value type')
+
+    raise Exception('Wrong value type')
 
 
 def translate_name(tree_name: dict) -> str:
@@ -35,10 +35,10 @@ def translate_operator(tree_operator: dict) -> str:
 def translate_varname(tree_varname: dict) -> str:
     if 'name' in tree_varname['var_name']:
         return translate_name(tree_varname['var_name'])
-    elif 'operator' in tree_varname['var_name']:
+    if 'operator' in tree_varname['var_name']:
         return translate_operator(tree_varname['var_name'])
-    else:
-        raise Exception('Wrong variable name format')
+
+    raise Exception('Wrong variable name format')
 
 
 def translate_assign(tree_assign: dict) -> str:
@@ -104,9 +104,9 @@ def translate_infix(tree_infix: dict) -> str:
     if 'var_name' in operation and (not 'operator' in operation['var_name']):
         bracked_arguments = [f'({argument})' for argument in arguments]
         return f"{translate_varname(operation)}{''.join(bracked_arguments)}"
-    else:
-        operator = translate_operator(operation['var_name'])
-        return f"({arguments[0]}) {operator} ({arguments[1]})"
+
+    operator = translate_operator(operation['var_name'])
+    return f"({arguments[0]}) {operator} ({arguments[1]})"
 
 
 def translate_applying(tree_app: dict) -> str:
@@ -123,25 +123,23 @@ def translate_applying(tree_app: dict) -> str:
 
 def translate_expr(tree_expr: dict) -> str:
     if type(tree_expr) is list:
-        if len(tree_expr) == 1:
-            tree_expr = tree_expr[0]
-            return translate_expr(tree_expr)
-        elif len(tree_expr) == 0:
+        if len(tree_expr) == 0:
             return ''
-        else:
-            return translate_applying(tree_expr)
+        if len(tree_expr) == 1:
+            return translate_expr(tree_expr[0])
+        return translate_applying(tree_expr)
 
     if 'value' in tree_expr:
         return translate_value(tree_expr)
-    elif 'assign' in tree_expr:
+    if 'assign' in tree_expr:
         return translate_assign(tree_expr)
-    elif 'import' in tree_expr:
+    if 'import' in tree_expr:
         return translate_import(tree_expr)
-    elif 'lambda' in tree_expr:
+    if 'lambda' in tree_expr:
         return translate_lambda(tree_expr)
-    elif 'var_name' in tree_expr:
+    if 'var_name' in tree_expr:
         return translate_varname(tree_expr)
-    elif 'infix' in tree_expr:
+    if 'infix' in tree_expr:
         return translate_infix(tree_expr)
 
     return str(tree_expr)
