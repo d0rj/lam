5bbaed0c1fb61bfa43e8c82500942c64b29b2e29
d0rj <hanaan@internet.ru>
2021-09-26 22:20:14 +0300

    refactor: move code samples to new folder
    Now main function just prints translated code. Some little
    part is done. Now language needs to be more complex

--- lambda.py
+++ lambda.py
@@ -1,13 +1,18 @@
+from inspect import isfunction, signature
+from types import BuiltinFunctionType
+
 from src.transformer import ToDictTransformer
 from src.parser import parse
 from src.translater import translate
 
 
 def main() -> None:
-    tree = parse('programm.l')
+    tree = parse('./examples/hello_world.l')
 
     new_tree = ToDictTransformer().transform(tree)
-    translate(new_tree)
+
+    for line in translate(new_tree):
+        print(line)
 
 
 if __name__ == '__main__':
